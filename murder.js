53a7c9cf329d59267e37c4d4b17306849ce9adde
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2019-02-19 17:02:34 +1100

    Update murder.js

--- murder.js
+++ murder.js
@@ -2,5 +2,5 @@
 document.addEventListener('click', function(e) {
     e = e || window.event;
     var target = e.target || e.srcElement;
-    target.style.backgroundColor = "red";
+    target.style.color = "red";
 }, false);
