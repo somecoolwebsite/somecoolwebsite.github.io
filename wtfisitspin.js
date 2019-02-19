1e45344745d093671e51b1958225eb1944813f20
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2019-02-19 17:27:07 +1100

    Update wtfisitspin.js

--- wtfisitspin.js
+++ wtfisitspin.js
@@ -1,10 +1,12 @@
 var target;
+var s = document.createElement("style");
+s.type='text/css';
+s.appendChild(document.createTextNode('@keyframes theepic {from {transform:rotate(0deg);}to {transform:rotate(360deg);}'));
+document.head.appendChild(s);
+
 document.addEventListener('click', function(e) {
     e = e || window.event;
     target = e.target || e.srcElement;
-    var s = document.createElement("style");
-    s.type='text/css';
-    s.appendChild(document.createTextNode('@keyframes theepic {from {transform:rotate(0deg);}to {transform:rotate(360deg);}'));
     target.style.animation = "theepic 1s infinite";
 
 }, false);
