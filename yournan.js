2cc2ddfad12cb4a6b1d404f16c919ad4a8703ff8
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2019-02-25 12:43:37 +1100

    Update yournan.js

--- yournan.js
+++ yournan.js
@@ -1,5 +1,5 @@
 document.addEventListener('click', function(e) {
     e = e || window.event;
     var target = e.target || e.srcElement;
-    target.outerHTML = "your nan";
+    target.innerHTML = "your nan";
 }, false);
