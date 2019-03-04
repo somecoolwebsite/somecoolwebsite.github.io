b19b703ab4b0fb53b99b06f9e692c2600363a1cd
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2019-03-04 13:42:27 +1100

    Update click.js

--- click.js
+++ click.js
@@ -11,7 +11,7 @@ var c = 0;
 var s = document.createElement("style");
 var g = document.createElement("div");
 g.id='gmaerrrrrrrrrr';
-g.innerHTML="<div id="gmaerrrrrrrrrr"><p>click</p><p id="clicks"></p><div>"
+g.innerHTML="<div id=\"gmaerrrrrrrrrr\"><p>click</p><p id=\"clicks\"></p><div>"
 s.type='text/css';
 s.appendChild(document.createTextNode('#gmaerrrrrrrrrr{position:"fixed";bottom:"10px";left:"10px;}#gmaerrrrrrrrrr:not(:hover){visibility: hidden;} #gmaerrrrrrrrrr:hover{visibility: visible;}'));
 document.head.appendChild(s);
