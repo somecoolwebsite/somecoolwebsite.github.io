732a9bae8d34a2159cffc874464e9d66e32a5346
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2019-03-04 14:03:39 +1100

    Update click.js

--- click.js
+++ click.js
@@ -19,4 +19,12 @@ document.body.appendChild(g);
 document.addEventListener('click', function(e) {
   c++;
   document.getElementById("clicks").innerHTML = c;
+  popp();
 }, false);
+
+function popp(){
+ for (i=0; i<10; i++){
+ var rndom_left = Math.round(Math.random() 500) % 500;
+ var rndom_top = Math.round(Math.random() 200) % 200;
+ document.write("<div id='coverup' class='cover' style='z-index:"+i+";position:relative;visibility:visible;left:"+rndom_left+"px;top:"+rndom_top+"px;width:50px;height:50px;color:red;'>what the actual backflip</div>");}
+}
