5814a631607df5ebe33f9dac90a1603cfd2c866c
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2019-03-05 08:46:25 +1100

    Update click.js

--- click.js
+++ click.js
@@ -1,4 +1,4 @@
-/*  _            _                          _  __     
+/*_            _                          _  __     
  | |          | |                        | |/ /     
  | | ___   ___| | ___ __   ___  ___ ___  | ' / ___  
  | |/ _ \ / __| |/ / '_ \ / _ \/ __/ __| |  < / _ \ 
@@ -7,6 +7,9 @@
  
 //A click gmae
  
+var epic = [];
+var documentHeight = document.documentElement.clientHeight;
+var documentWidth = document.documentElement.clientWidth;
 var c = 0;
 var s = document.createElement("style");
 var g = document.createElement("div");
@@ -17,6 +20,11 @@ s.type='text/css';
 document.head.appendChild(s);
 document.body.appendChild(g);
 document.addEventListener('click', function(e) {
+  var currentTop = Math.floor(Math.random() * documentHeight) + 1;
+  var currentLeft = Math.floor(Math.random() * documentWidth) + 1;
+  epic.push(document.createElement("div"));
+  epic[epic.length].style.top = currentTop + "px";
+  epic[epic.length].style.left = currentLeft + "px";
   c++;
   document.getElementById("clicks").innerHTML = c;
 }, false);
