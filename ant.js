90194453272759687f08eeb160511376bb294f05
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2019-03-06 08:22:26 +1100

    Update ant.js

--- ant.js
+++ ant.js
@@ -2,6 +2,20 @@
 
 //this is a very annoy for your compute
 
+var documentHeight = document.documentElement.clientHeight;
+var documentWidth = document.documentElement.clientWidth;
 var nest = document.createElement('div');
+var ants = [];
 nest.id = "nest";
-nest.innerHTML = "";
+nest.innerHTML = "<img src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTzT_5FXEOPLi654Di8TNxpd5UAtHzb9QfeW6WzdSVdsQblO6X5\"></img>";
+nest.style.position = "absolute";
+var currentTop = Math.floor(Math.random() * documentHeight) + 1;
+var currentLeft = Math.floor(Math.random() * documentWidth) + 1;
+nest.style.top = currentTop + "px";
+nest.style.left = currentLeft + "px";
+nest.style.zIndex = 2;
+document.body.appendChild(nest);
+document.addEventListner('click', function(e) {
+  ants.push(document.createElement('p'));
+  ants[ants.length-1].outerHTML = "<p>🐜</p>";
+}, false);
