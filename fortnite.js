8222ea7208f523ddf13c85273dc34ede8629829a
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2019-03-05 19:18:05 +1100

    Update fortnite.js

--- fortnite.js
+++ fortnite.js
@@ -1,1 +1,1 @@
-document.body.innerHTML = "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/AqpYHdv3S00?&autoplay=1\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
+document.body.innerHTML = "<iframe width="+window.innerWidth+" height="+window.innerHeight+" src=\"https://www.youtube.com/embed/AqpYHdv3S00?&autoplay=1\" frameborder=\"0\" allow=\"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture\" allowfullscreen></iframe>";
