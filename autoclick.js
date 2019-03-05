9f23437b20773b50757dafddac6565631d98eda1
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2019-03-05 13:48:53 +1100

    Update autoclick.js

--- autoclick.js
+++ autoclick.js
@@ -1,14 +1,1 @@
-var sl = document.createElement('input'):
-var cps = 0;
-sl.id = 'lll';
-sl.type = 'range';
-sl.min = 1;
-sl.max = 100;
-sl.oninput = function() {
-  cps = this.value;
-}
-while (true){
-  for(i = 0; i<cps; i++){
-    document.body.click();
-  }
-}
+setInterval(function(){document.body.click(); }, 10);
