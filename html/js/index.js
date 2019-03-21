f5207732c342859071224a9a6574cb44e4ec5886
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2019-03-21 17:18:04 +1100

    comments

--- html/js/index.js
+++ html/js/index.js
@@ -7,11 +7,10 @@ function setup() {
 	createCanvas(640, 480);
 	video = createCapture(VIDEO);
 	video.size(width, height);
-
-	// Create a new poseNet method with a single detection
+	
+	//makes a knew pose instance
 	poseNet = ml5.poseNet(video, modelReady);
-	// This sets up an event that fills the global variable "poses"
-	// with an array every time new poses are detected
+	//does some detection stuff
 	poseNet.on('pose', function (results) {
 		poses = results;
 	});
@@ -24,20 +23,16 @@ function modelReady() {
 function draw() {
 	image(video, 0, 0, width, height);
 
-	// We can call both functions to draw all keypoints and the skeletons
+	//draw the spooky scary skelertons that send shivers down ur spine
 	drawKeypoints();
 	drawSkeleton();
 }
 
-// A function to draw ellipses over the detected keypoints
 function drawKeypoints()  {
-	// Loop through all the poses detected
 	for (let i = 0; i < poses.length; i++) {
-		// For each pose detected, loop through all the keypoints
 		for (let j = 0; j < poses[i].pose.keypoints.length; j++) {
-			// A keypoint is an object describing a body part (like rightArm or leftShoulder)
 			let keypoint = poses[i].pose.keypoints[j];
-			// Only draw an ellipse is the pose probability is bigger than 0.2
+			//only draw the thing if the ai isnt bullsh*ting u
 			if (keypoint.score > 0.2) {
 				fill(255, 0, 0);
 				noStroke();
@@ -47,12 +42,11 @@ function drawKeypoints()  {
 	}
 }
 
-// A function to draw the skeletons
+//spoopy skeletons
 function drawSkeleton() {
-	// Loop through all the skeletons detected
 	for (let i = 0; i < poses.length; i++) {
-		// For every skeleton, loop through all body connections
 		for (let j = 0; j < poses[i].skeleton.length; j++) {
+			//literally just drawing lines
 			let partA = poses[i].skeleton[j][0];
 			let partB = poses[i].skeleton[j][1];
 			stroke(255, 0, 0);
