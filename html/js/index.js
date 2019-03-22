24cbe6b387927fd3c5f4ccf562ead329e882590f
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2019-03-22 14:00:34 +1100

    Update index.js

--- html/js/index.js
+++ html/js/index.js
@@ -28,7 +28,7 @@ function draw() {
 	if(jp===false){
 		scale(-1.0,1.0);
 		imageMode(CORNER);
-		image(video, 0, 0, width, height);
+		image(video, width, 0, width, height);
 	}
 	else{scale(1.0,1.0);}
 	//draw the spooky scary skelertons that send shivers down ur spine
