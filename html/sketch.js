fc480d8370da73fa0a8269901d02e58b83a7bad1
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2019-04-08 14:37:41 +1000

    Update sketch.js

--- html/sketch.js
+++ html/sketch.js
@@ -1,4 +1,4 @@
-let brian = new NeuralNetwork(8, 10, 1);
+let brian = new NeuralNetwork(7, 10, 1);
 let iterations = 0;
 let ops = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
 let s;
