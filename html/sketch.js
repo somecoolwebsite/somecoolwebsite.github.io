d6a2653233b576ed567d13c0687937f273e02a20
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2019-04-08 14:34:54 +1000

    Update sketch.js

--- html/sketch.js
+++ html/sketch.js
@@ -1,4 +1,4 @@
-let brian = new NeuralNetwork(8, 10, 7);
+let brian = new NeuralNetwork(8, 10, 1);
 let iterations = 0;
 let ops = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
 let s;
@@ -144,7 +144,7 @@ function draw() {
   iterations = s.value();
   for(var i=0;i<iterations;i++){
     let data = random(training_data);
-    brian.train([data.inputs,data.outputs]);
+    brian.train(data.inputs,data.outputs);
   }
   fill('white');
   text(iterations+" itertions", 10, 10);
