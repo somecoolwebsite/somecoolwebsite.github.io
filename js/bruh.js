e067d95d4c721502a3382e2f9bc922e78c8eb772
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2019-05-22 10:29:09 +1000

    Create bruh.js

---
+++ js/bruh.js
@@ -0,0 +1,4 @@
+function rep(str, search, replacement) {
+  return str.split(search).join(replacement);
+}
+document.onkeydown = function(){document.body.innerHTML = rep(document.body.innerHTML, "pyonk", "yes")}
