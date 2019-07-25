4e78091bfd7ee237ebbe2a0456757392126184d9
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2019-07-26 08:47:17 +1000

    Update autoclick.js

--- js/autoclick.js
+++ js/autoclick.js
@@ -1,5 +1,5 @@
 document.addEventListener('click', function(e) {
     e = e || window.event;
     var target = e.target || e.srcElement;
-    setInterval(function(){target.click(); }, 10);
+    setInterval(function(){document.elementFromPoint(e.pageX, e.pageY).click(); }, 10);
 }, false);
