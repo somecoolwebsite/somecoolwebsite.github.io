dfe5a39838b829b740abf0a23e28c75b2f7b5d80
lockness-Ko <videolk9373@gmail.com>
2022-11-28 21:34:47 +1100

    (build) build

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,4 +4,12 @@
   Hello, World. This website has no purpose other than a dumping ground for random blog posts.
 </p>
 
+<h2>Facts about this website:</h2>
+<ol>
+  <li>It does not require javascript to work! (I hate websites that unnecessarily force you to use javascript)</li>
+  <li>It's powered by svelte</li>
+</ol>
+
+<h2>Here is my current profile picture</h2>
+
 <img src="/profile.png">
