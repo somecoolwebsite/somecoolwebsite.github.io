cf98f2535da46d3de2a7abad05e7cc0d8a355d22
lockness-Ko <videolk9373@gmail.com>
2022-11-28 14:51:22 +1100

    (update) configure svelte build config

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,7 +8,14 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter({ out: 'docs' })
+		adapter: adapter({
+			pages: 'docs',
+      assets: 'docs',
+      fallback: null,
+      precompress: true,
+      strict: true,
+			trailingSlash: 'always'
+		})
 	}
 };
 
