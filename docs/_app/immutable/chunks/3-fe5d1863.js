a17238065fb461ffce7189218f8848bf6c07b232
lockness-Ko <videolk9373@gmail.com>
2024-11-03 11:08:24 +1100

    (fix): actually commit the site

--- docs/_app/immutable/chunks/3-fe5d1863.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/about/_page.svelte-a43a9ab9.js";export{t as component};
--- docs/_app/immutable/chunks/6-c68d1a3a.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/contact/_page.svelte-632ef770.js";export{t as component};
