7b7aff70d4dbd17c5956ce410ccb43270029702d
lockness-Ko <videolk9373@gmail.com>
2024-11-03 11:03:38 +1100

    (fix): contact deets

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,6 +1,3 @@
 <h1>Hi I'm lockness Ko!</h1>
 
 <p>This is all about me</p>
-
-<a href="https://x.com/locknessko">@locknessko twitter</a>
-<a href="https://github.com/lockness-Ko">@lockness-Ko github</a>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,6 +1,9 @@
 <h1>Talk to me</h1>
 
 <p>
-  <a href="mailto:contact@lockness.pro">Send me an email</a>
-  Discord: lockness Ko#7954
+  <a href="https://x.com/locknessko">twitter</a>
+  <br/>
+  <a href="https://github.com/lockness-Ko">github</a>
+  <br/>
+  Discord: locknessko
 </p>
